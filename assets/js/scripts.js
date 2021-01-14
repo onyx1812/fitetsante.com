4eb9abca34ed2b85e0c06764029d5998a7250cfd
massagehero <onyx18121990@gmail.com>
2021-01-14 09:51:51 +0200

    added track link

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,14 @@
 (function(){
 
+  const trackClick = document.getElementById('trackClick'),
+    trackLink = 'https://sakmtracker.com/click',
+    queryLink = window.location.search,
+    newTrackLink = trackLink + queryLink;
+
+  trackClick.addEventListener('click', e => {
+    e.preventDefault();
+    window.location.href = newTrackLink;
+  });
+
+
 })();
--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -1,69 +1,69 @@
 'use strict';
 
 var
-	gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	htmlmin = require('gulp-htmlmin'),
-	imagemin = require('gulp-imagemin'),
-	pngquant = require('imagemin-pngquant'),
-	rigger = require('gulp-rigger'),
-	browserSync = require('browser-sync').create();
+  gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  htmlmin = require('gulp-htmlmin'),
+  imagemin = require('gulp-imagemin'),
+  pngquant = require('imagemin-pngquant'),
+  rigger = require('gulp-rigger'),
+  browserSync = require('browser-sync').create();
 
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
-	return gulp.src('src/scss/**/*.scss')
-		.pipe(sass.sync({outputStyle: 'uncompressed'})
-		.on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}))
-		.pipe(gulp.dest('../assets/css'))
-		.pipe(browserSync.reload({stream: true}));
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass.sync({outputStyle: 'compressed'})
+    .on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('../assets/css'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('html', function() {
-	return gulp.src('src/*.html')
-		.pipe(rigger())
-		.pipe(htmlmin({collapseWhitespace: false}))
-		.pipe(gulp.dest('../'))
-		.pipe(browserSync.reload({stream: true}));
+  return gulp.src('src/*.html')
+    .pipe(rigger())
+    .pipe(htmlmin({collapseWhitespace: false}))
+    .pipe(gulp.dest('../'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('js', function(){
-	return gulp.src('src/js/**/*.js')
-		.pipe(rigger())
-		.pipe(gulp.dest('../assets/js'))
-		.pipe(browserSync.reload({stream: true}));
+  return gulp.src('src/js/**/*.js')
+    .pipe(rigger())
+    .pipe(gulp.dest('../assets/js'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('images', function () {
-	return gulp.src('src/img/**')
-		.pipe(imagemin({
-			progressive: true,
-			svgoPlugins: [{removeViewBox: false}],
-			use: [pngquant()],
-			interlaced: true
-		}))
-		.pipe(gulp.dest('../assets/img'));
+  return gulp.src('src/img/**')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()],
+      interlaced: true
+    }))
+    .pipe(gulp.dest('../assets/img'));
 });
 
 gulp.task('browser-sync', function() {
-	browserSync.init({
-		server: {
-			baseDir: "../"
-		},
-		notify: false,
-		port: 5050
-	});
+  browserSync.init({
+    server: {
+      baseDir: "../"
+    },
+    notify: false,
+    port: 5050
+  });
 });
 
 gulp.task('default', ['browser-sync', 'sass', 'html', 'images', 'js'], function () {
-	gulp.watch('src/scss/**/*.scss', ['sass']);
-	gulp.watch('src/**/*.html', ['html']);
-	gulp.watch('src/img/**', ['images']);
-	gulp.watch('src/js/**/*.js', ['js']);
+  gulp.watch('src/scss/**/*.scss', ['sass']);
+  gulp.watch('src/**/*.html', ['html']);
+  gulp.watch('src/img/**', ['images']);
+  gulp.watch('src/js/**/*.js', ['js']);
 });
