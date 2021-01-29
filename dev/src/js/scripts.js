ce03b94e4cd01e1461950bb19e40df3bb8d5ae03
massagehero <onyx18121990@gmail.com>
2021-01-29 11:44:34 +0200

    added cope changes

--- dev/src/js/scripts.js
+++ dev/src/js/scripts.js
@@ -10,5 +10,26 @@
     window.location.href = newTrackLink;
   });
 
+  const popups = document.querySelectorAll('[data-popup]');
+  popups.forEach(popup => {
+    popup.addEventListener('click', e => {
+      e.preventDefault();
+      const popupContent = document.getElementById(e.target.dataset.popup).innerHTML;
+      document.querySelector('.popup').classList.add('popup-active');
+      document.querySelector('.popup-content').innerHTML = popupContent;
+      document.body.style.overflowY = 'hidden';
+      document.body.style.marginRight = '17px';
+    });
+  });
+
+  const popupClose = document.querySelectorAll('.popup-close');
+  popupClose.forEach(close => {
+    close.addEventListener('click', e => {
+      e.preventDefault();
+      document.querySelector('.popup').classList.remove('popup-active');
+      document.body.style.overflowY = 'auto';
+      document.body.style.marginRight = '0';
+    });
+  });
 
 })();
